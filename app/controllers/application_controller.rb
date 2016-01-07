ea47b8c52ee9f5a95d2fef1183b02d74bf925afd
Hemantk <hemant_14j83@yahoo.co.in>
2016-01-07 06:46:22 +0000

    Adding Merchant Notifier and admin panel link in profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_u_permitted_parameters, if: :devise_controller?
+  #before_filter :after_sign_in_path_for, if: :devise_controller?
+  #before_filter :after_sign_out_path_for, if: :devise_controller?
 
   private
 
@@ -14,13 +16,13 @@ class ApplicationController < ActionController::Base
   	cart
   end
 
-
   protected
     def configure_u_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) do |t|
       t.permit(:fname,:email,:encrypted_password,:lname)      
     end
 
+    
     #devise_parameter_sanitizer.for(:sign_up) do |t|
     #  t.permit(:email, :encrypted_password,:fname,:lname)
     #end
